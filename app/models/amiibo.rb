b5a475822b5d1119fb2e6c14de5aa6f594b9b4ff
Stephann Vasconcelos <stephannvasconcelos@gmail.com>
2015-12-27 02:05:22 -0300

    Creating seeds for Amiibo, Series and Company

--- app/models/amiibo.rb
+++ app/models/amiibo.rb
@@ -1,10 +1,7 @@
 class Amiibo < ApplicationRecord
 	belongs_to :company
-	belongs_to :series
+	belongs_to :series, optional: true
 	
-	validates_presence_of :name, :release_date, :company_id, 
-		:description, :detail_image_url, :boxart_image_url
-	validates_uniqueness_of :name, scope: :series_id
-
-
+	validates_presence_of :name, :release_date, :company_id,  :description, :detail_image_url, :boxart_image_url
+	validates :name , uniqueness: { scope: :series_id }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+amiibos = ActiveSupport::JSON.decode(File.read('db/amiibos.json'))
+
+amiibo_count = Amiibo.count
+series_count = Series.count
+company_count = Company.count
+
+amiibos.each do |amiibo|
+	amiibo['company_id'] = Company.find_or_create_by!( name: amiibo['company'] ).id
+	amiibo['series_id'] = Series.find_or_create_by( name: amiibo['series'] ).id || nil
+	amiibo.except!( 'series', 'company' )
+	
+	Amiibo.find_or_create_by!( amiibo )
+end
+
+pp "Created #{Company.count - company_count} companies"
+pp "Created #{Series.count - series_count} series"
+pp "Created #{Amiibo.count - amiibo_count} amiibos"
